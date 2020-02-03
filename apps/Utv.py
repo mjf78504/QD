461ec8d9f47817b23c242b3dc4618e76b8e058c1
mjf78504 <admin@xinwl.com>
2020-02-03 17:09:46 +0800

    Update Utv.py

--- apps/Utv.py
+++ apps/Utv.py
@@ -14,21 +14,20 @@ class utvAPP:
         }
 
     def utv(self, username, userid):
-      	 self.phoneNum = username
-      	 self.phoneId = userid
-    	 login_url = 'http://campaign.utvhk.com/jfdk/login.do'
-    	 login_data = {
-    	     "deviceID": "8e6aab4ec4c41e16d2933474169e8a1e",
-    	     "isBuySpecialProductPackage": [],
-    	     "isCMHKUser": "true",
-    	     "lastLoginTime": datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],
-    	     
-    	     "nickName": "52****76",
-    	     "registeTime": "2020-02-02T18:51:13",
-    	     "userID": self.phoneId,
-    	     "userPhone": self.phoneNum,
-    	     "watchTime": []
-    	  }
+        self.phoneNum = username
+        self.phoneId = userid
+      	login_url = 'http://campaign.utvhk.com/jfdk/login.do'
+      	login_data = {
+            "deviceID": "8e6aab4ec4c41e16d2933474169e8a1e",
+    	    "isBuySpecialProductPackage": [],
+    	    "isCMHKUser": "true",
+    	    "lastLoginTime": datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],
+    	    "nickName": "52****76",
+    	    "registeTime": "2020-02-02T18:51:13",
+    	    "userID": self.phoneId,
+    	    "userPhone": self.phoneNum,
+            "watchTime": []
+        }
         login_req = self.session.post(login_url, data=login_data, headers=self.headers)
         login_msg = '登录情况：' + login_req.json()['msg'] + '\n'
         return login_msg
